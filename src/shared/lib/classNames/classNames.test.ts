ab84ba9d470b57b742083ac2bdc07299a07022e0
Alex-Berezov <aleber197@gmail.com>
2023-08-17 10:22:24 +0400

    add first test

--- src/shared/lib/classNames/classNames.test.ts
+++ src/shared/lib/classNames/classNames.test.ts
@@ -1,7 +1,42 @@
-import { describe } from 'node:test'
+import { classNames } from './classNames'
 
 describe('classNames', () => {
-  test('test', () => {
-    expect(true).toBe(true)
+  test('with only first param', () => {
+    expect(classNames('someClass')).toBe('someClass')
+  })
+
+  test('with additional classes', () => {
+    const expected = 'someClass class1 class2'
+    expect(classNames('someClass', {}, ['class1', 'class2'])).toBe(expected)
+  })
+
+  test('with mods', () => {
+    const expected = 'someClass class1 class2 hovered scrollable'
+    expect(
+      classNames('someClass', { hovered: true, scrollable: true }, [
+        'class1',
+        'class2',
+      ])
+    ).toBe(expected)
+  })
+
+  test('with mods', () => {
+    const expected = 'someClass class1 class2 hovered'
+    expect(
+      classNames('someClass', { hovered: true, scrollable: false }, [
+        'class1',
+        'class2',
+      ])
+    ).toBe(expected)
+  })
+
+  test('with mods', () => {
+    const expected = 'someClass class1 class2 hovered'
+    expect(
+      classNames('someClass', { hovered: true, scrollable: undefined }, [
+        'class1',
+        'class2',
+      ])
+    ).toBe(expected)
   })
 })
