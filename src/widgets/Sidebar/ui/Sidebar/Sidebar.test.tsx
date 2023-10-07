e0ece5735231d6f69aa5c38811bae891841424a8
Alex-Berezov <aleber197@gmail.com>
2023-10-07 17:43:48 +0400

    fixed tests

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,18 +1,27 @@
 import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { fireEvent, render, screen } from '@testing-library/react'
+import { I18nextProvider } from 'react-i18next'
 import { Sidebar } from 'widgets/Sidebar'
+import i18n from 'shared/config/i18n/i18n'
 
 describe('Sidebar', () => {
+  const renderWithProviders = (component: React.ReactNode) => {
+    return render(
+      <I18nextProvider i18n={i18n}>
+        <Router>{component}</Router>
+      </I18nextProvider>
+    )
+  }
+
   test('Render component', () => {
-    render(<Sidebar />)
+    renderWithProviders(<Sidebar />)
 
     expect(screen.getByTestId('sidebar')).toBeInTheDocument()
   })
 
   test('Test toggle button', () => {
-    render(<Sidebar />)
-
-    expect(screen.getByTestId('sidebar')).toBeInTheDocument()
+    renderWithProviders(<Sidebar />)
 
     const toggleBtn = screen.getByTestId('sidebarToggle')
     fireEvent.click(toggleBtn)
