dd536683fbed353868b7633982486e2aef7a1a4e
Alex-Berezov <aleber197@gmail.com>
2024-02-29 10:59:54 +0400

    Remove unused import statements in AricleDetailsPage and ArticlesPage components

--- src/pages/AricleDetailsPage/ui/AricleDetailsPage/AricleDetailsPage.tsx
+++ src/pages/AricleDetailsPage/ui/AricleDetailsPage/AricleDetailsPage.tsx
@@ -1,15 +1,12 @@
 import { FC, memo } from 'react'
 import { classNames } from 'shared/lib/classNames/classNames'
 import cls from './AricleDetailsPage.module.scss'
-import { useTranslation } from 'react-i18next'
 
 interface AricleDetailsPageProps {
   className?: string
 }
 
 const AricleDetailsPage: FC<AricleDetailsPageProps> = ({ className }) => {
-  const { t } = useTranslation('article')
-
   return (
     <div className={classNames(cls.AricleDetailsPage, {}, [className])}>
       AricleDetailsPage
--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -1,15 +1,12 @@
 import { FC, memo } from 'react'
 import { classNames } from 'shared/lib/classNames/classNames'
 import cls from './ArticlesPage.module.scss'
-import { useTranslation } from 'react-i18next'
 
 interface ArticlesPageProps {
   className?: string
 }
 
 const ArticlesPage: FC<ArticlesPageProps> = ({ className }) => {
-  const { t } = useTranslation('article')
-
   return (
     <div className={classNames(cls.ArticlesPage, {}, [className])}>
       ArticlesPage
